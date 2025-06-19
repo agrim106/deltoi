e11a9da0f6209254ed71f65b373865651054ea96
Agrim Chaudhary <agrim.chaudhary@magicsw.com>
2025-06-20 02:56:53 +0530

    [Agrim]ADD: Initial Commit for query tracker

---
+++ src/main/java/com/deltoi/app/DeltoiApplication.java
@@ -0,0 +1,13 @@
+package com.deltoi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deltoi/app/DeltoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltoi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
